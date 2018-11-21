47e5d80010648666b25b5c91ec3e0b9bb3704ea3
Sam Fader <sam.fader@mapbox.com>
2018-11-20 23:24:27 -0500

    add more locations

--- map.js
+++ map.js
@@ -27,98 +27,6 @@ map.on("load", function() {
 		clusterRadius: 50
 	});
 
-	// // FILTERING ----
-	// // TO DO: this should be the data in sounds somehow
-	// var sounds = {
-	//   "features": [
-	//     {
-	//       "type": "Feature",
-	//       "properties": {
-	//         "title": "Pacific Ocean at Cascade Head",
-	//         "recording_device": "Portable Zoom iPhone",
-	//         "html": "<p><audio width='300' height='32' src='https://dl.dropboxusercontent.com/s/tps1xqh28o1mqad/mp3Pacific%20Ocean%20At%20Cascade%20Head.mp3?' controls='controls'><br />Your browser does not support the audio element.<br /></audio></p>"
-	//       },
-	//       "geometry": {
-	//         "coordinates": [
-	//           -124.009127,
-	//           45.05322
-	//         ],
-	//         "type": "Point"
-	//       },
-	//       "id": "8d41e04b65554ad2ee1781b9a6640b83"
-	//     },
-	//     {
-	//       "type": "Feature",
-	//       "properties": {
-	//         "title": "Bridge on Clackamas River Trail Near Indian Henry Campground",
-	//         "description": "",
-	//         "recording_device": "Portable Zoom iPhone",
-	//         "html": "<p><audio width='300' height='32' src='https://dl.dropboxusercontent.com/s/8xxkmw4wdru23j3/Bridge%20On%20Clackamas%20River%20Trail%20Near%20Indian%20Henry%20Campground%20%281%29.mp3?' controls='controls'><br />Your browser does not support the audio element.<br /></audio></p>"
-	//       },
-	//       "geometry": {
-	//         "coordinates": [
-	//           -122.079219,
-	//           45.124683
-	//         ],
-	//         "type": "Point"
-	//       },
-	//       "id": "ef95770ed9337c2f4b71b71379231c0b"
-	//     }
-	//   ],
-	//   "type": "FeatureCollection"
-	// }
-
-	// // filtering
-	// sounds.features.forEach(function(feature) {
-	// 	// TO DO: add error handling in here in case year does not exist
-	// 	var year = feature.properties['year'];
-	// 	var layerID = 'year-' + year;
-
-	// 	// add layer for the year if it hasn't been
-	// 	if (!map.getLayer(layerID)) {
-	// 		map.addLayer({
-	// 			id: layerID,
-	// 			type: 'circle',
-	// 			source: 'sounds',
-	// 			// TO DO: fix this, this filter is causing the points not to show up
-	// 			// filter: ['all', ['has', 'point_count'], ['==', 'year', year]],
-	// 	    paint: {
-	// 	    	'circle-color': '#000000',
-	// 	    }
-	// 		});
-	// 	}
-
-	// 	var input = document.createElement('input');
-	// 	input.type = 'checkbox';
-	// 	input.id = layerID;
-	// 	input.checked = true;
-	// 	filterGroup.appendChild(input);
-
-	// 	var label = document.createElement('label');
-	// 	label.setAttribute('for', layerID);
-	// 	label.textContent = year;
-	// 	filterGroup.appendChild(label);
-
-	// 	// When the checkbox changes, update layer visibility
-	// 	input.addEventListener('change', function(e) {
-	// 		console.log(e.target.checked);
-	// 		map.setLayoutProperty(layerID, 'visibility', e.target.checked ? 'visible' : 'none');
-	// 		console.log(map.getLayoutProperty(layerID, 'visibility'));
-	// 	});
-
-	// 	// TO DO: make sure this is working
-	// 	map.on('mouseenter', layerID, function() {
-	// 		map.getCanvas().style.cursor = 'pointer';
-	// 	});
-
-	// 	map.on('mouseleave', layerID, function() {
-	// 		map.getCanvas().style.cursor = '';
-	// 	});
-	// });
-
-	// add clustering
-	// TO DO: fix this, think it needs to be changed to circle as well?
-
 	map.addLayer({
 			 id: "clusters",
 			 type: "circle",
@@ -170,14 +78,15 @@ map.on("load", function() {
 		source: "sounds",
 		filter: ["!has", "point_count"],
     layout: {
-	    "icon-image": "music-11",
-	    "icon-padding": 0,
+	    "icon-image": "music-15",
+      "icon-padding": 0,
+      "icon-size": 1.5,
 	    "icon-allow-overlap":true
 		}
 	});
 });
 
-map.on("click", function(e) {
+map.on('click', 'unclustered-point', function(e) {
 	var features = map.queryRenderedFeatures(e.point, {});
 
 	if (!features.length) {
@@ -187,7 +96,6 @@ map.on("click", function(e) {
 	var feature = features[0];
 
 	// fly to point when clicked
-
 	map.flyTo({ center: feature.geometry.coordinates });
 
 	// popup with mp3 and info
@@ -203,4 +111,12 @@ map.on("click", function(e) {
 		)
 		.setLngLat(feature.geometry.coordinates)
 		.addTo(map);
+});
+
+map.on('mouseenter', 'unclustered-point', function() {
+  map.getCanvas().style.cursor = 'pointer';
+});
+
+map.on('mouseleave', 'unclustered-point', function() {
+  map.getCanvas().style.cursor = '';
 });
