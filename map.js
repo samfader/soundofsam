f4f361db78888f4571c9d0b3619c49b2881900bc
Sam Fader <sam.fader@mapbox.com>
2019-08-22 15:35:31 -0700

    new stuff

--- map.js
+++ map.js
@@ -4,11 +4,23 @@ mapboxgl.accessToken =
 // set up filter group for filtering of items by year
 var filterGroup = document.getElementById("filter-group");
 var map = new mapboxgl.Map({
-	container: "map", // container id
-	style: "mapbox://styles/samf/cjoqivx4v1eae2snu8xfbf5jg", // stylesheet location
-	center: [-99.782286, 38.408858], // starting position
-  zoom: 3.6 // starting zoom
+	container: "map",
+	style: "mapbox://styles/samf/cjzk8fx125nc61cnss6fa6zp5",
+	center: [-99.782286, 38.408858],
+  zoom: 3.6,
+  hash: true
 });
+
+map.addControl(new MapboxGeocoder({
+  accessToken: mapboxgl.accessToken,
+  collapsed: true,
+  mapboxgl: mapboxgl
+}));
+
+map.addControl(new mapboxgl.NavigationControl());
+
+var language = new MapboxLanguage();
+map.addControl(language);
 
 // Build list for sidebar
 function buildYearList(data) {
@@ -18,7 +30,25 @@ function buildYearList(data) {
 	}
 }
 
-map.on("load", function() {
+var framesPerSecond = 21; 
+var initialRotation = 12;
+var rotation = initialRotation;
+var maxRotation = 360;
+
+var layerList = document.getElementById('menu');
+var inputs = layerList.getElementsByTagName('input');
+ 
+function switchLayer(layer) {
+var layerId = layer.target.id;
+map.setStyle('mapbox://styles/samf/' + layerId);
+}
+ 
+for (var i = 0; i < inputs.length; i++) {
+inputs[i].onclick = switchLayer;
+}
+
+map.on("style.load", function() {
+
 	map.addSource("sounds", {
 		type: "geojson",
 		data: "./features.geojson",
@@ -84,7 +114,7 @@ map.on("load", function() {
 	    "icon-image": "blue-music-15",
       "icon-padding": 0,
       "icon-size": 1.25,
-      "icon-rotate": 12,
+      "icon-rotate": initialRotation,
       "icon-allow-overlap":true,
       "text-field": "",
       "text-font": [
@@ -92,7 +122,7 @@ map.on("load", function() {
         "Arial Unicode MS Bold"
       ]
 		}
-	});
+  });
 });
 
 map.on('click', 'unclustered-point', function(e) {
@@ -124,7 +154,23 @@ map.on('click', 'unclustered-point', function(e) {
 
 map.on('mouseenter', 'unclustered-point', function() {
   map.getCanvas().style.cursor = 'pointer';
-});
+  function animateMarker(timestamp) {
+    setTimeout(function(){
+        requestAnimationFrame(animateMarker);
+
+        rotation += (maxRotation - rotation) / framesPerSecond;
+
+        map.setLayoutProperty('unclustered-point', 'icon-rotate', rotation);
+
+        if (rotation <= 0) {
+            rotation = initialRotation;
+        } 
+
+    }, 1000 / framesPerSecond);
+  }
+    // Start the animation.
+    animateMarker(0);
+    });
 
 map.on('mouseleave', 'unclustered-point', function() {
   map.getCanvas().style.cursor = '';
